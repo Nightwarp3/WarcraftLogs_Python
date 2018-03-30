b5c84fc82fb102421c48fcde123875c5d2e9ae99
Mike Campbell <swimmerboi93@hotmail.com>
2018-03-30 00:53:22 -0600

    fixed some bugs, set up app.py, some additional work on MainPage.css and .html

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from WarcraftLogsPython import main
+from WarcraftLogsPython.main import app
 from flask import Flask
 
-app = Flask(__name__)
 
-@app.route('/')
-def app():
-  return main.app()
+if __name__ == "__main__":
+    app.run()
