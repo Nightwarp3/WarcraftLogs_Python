d31ae4bff67139b4df6207241232a90d00cbf16d
Night Wrap <36970221+Nightwarp3@users.noreply.github.com>
2018-03-30 10:21:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from WarcraftLogsPython.main import app
+#from WarcraftLogsPython.main import app
 from flask import Flask
 
+app = Flask(__name__)
+
+app.route('/')
+def mainpage():
+    return ('Helllflakjdf;aljef;lawfjasl;ef')
 
 if __name__ == "__main__":
     app.run()
