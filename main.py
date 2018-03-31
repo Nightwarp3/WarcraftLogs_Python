0baed82c891b277572464028236e3893c136e866
Mike Campbell <swimmerboi93@hotmail.com>
2018-03-30 18:00:13 -0600

    Added icons and buttons.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask import render_template
+from WarcraftLogsPython import LogsAPIRequests as la
 
 app = Flask(__name__)
 
 @app.route('/')
-def main_page():
+def MainPage():
     return render_template('MainWebPage.html')
+
+@app.route('/getChar')
+def get_char():
+    character = la.getCharacterRanking()
 
 if __name__ == "__main__":
     app.run()
