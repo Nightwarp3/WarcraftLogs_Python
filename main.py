262bd4356793ffef71050ba9ae7dfea0aa935d2b
Night Wrap <36970221+Nightwarp3@users.noreply.github.com>
2018-03-30 18:05:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from flask import Flask
 from flask import render_template
-from WarcraftLogsPython import LogsAPIRequests as la
 
 app = Flask(__name__)
 
 @app.route('/')
 def MainPage():
     return render_template('MainWebPage.html')
-
-@app.route('/getChar')
-def get_char():
-    character = la.getCharacterRanking()
 
 if __name__ == "__main__":
     app.run()
