24aea51e338a791d8e12e074b3e98c5320feb6c8
Mike Campbell <swimmerboi93@hotmail.com>
2018-04-02 14:54:25 -0600

    updated to use base template for html with page changes for each tab.

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def MainPage():
-    return render_template('MainWebPage.html')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/report_results')
+def report_results():
+    return render_template('reportresults.html')
+
+@app.route('/fight_results')
+def fight_results():
+    return render_template('fightresults.html')
+
+@app.route('/char_rank')
+def char_rank():
+    return render_template('charrank.html')
 
 if __name__ == "__main__":
     app.run()
